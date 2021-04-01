74debd8d83e21bd5c46bb2af745ff0a28b257bcf
lgarber48 <lgarber48@gmail.com>
2021-04-01 17:27:17 -0400

    Improved Plotting Section

--- Watch_Workouts.py
+++ Watch_Workouts.py
@@ -41,11 +41,18 @@ def extract_workout(i):
     Total_Time=End_time-Start_time
     Workout_Type=df_workouts['@workoutActivityType'].iloc[i]
 
+    #Convert workout duration to seconds (from TimeDelta)
+    Time_Comp=Total_Time.components
+    Wk_Duration_Seconds=Time_Comp[0]*86400 + Time_Comp[1]*3600+ Time_Comp[2]*60 + Time_Comp[3] + Time_Comp[4]/1000
+    #Convert to Workout Duration to int
+    Wk_Duration_Seconds=int(Wk_Duration_Seconds)
+
     print('Workout Type: '+str(Workout_Type))
     print('Workout Start Time: '+str(Start_time))
     print('Workout End Time: '+str(End_time))  
     print('Workout Duration: '+str(Total_Time))
-    return Start_time, End_time, Total_Time, Workout_Type
+    print('Total Workout Duration in Seconds: '+str(Wk_Duration_Seconds))
+    return Start_time, End_time, Wk_Duration_Seconds, Workout_Type
 
     #Save as CSV
     #finalADS=df_workouts.to_csv('/Users/louisgarber/Documents/McMaster Biomedical Eng/Apple Watch Project/apple_health_export/finalADS.csv',header=True)
@@ -72,41 +79,9 @@ def extract_HR(Wk_Data):
     #print(df_records_HR_Wk.head(10))
     #print(df_records_HR_Wk.iloc[3])
 
-    #Convert workout duration to seconds (from TimeDelta)
-    Wk_Duration=Wk_Data[2]
-    print('Total Workout Duration: '+str(Wk_Duration))
-    Time_Comp=Wk_Duration.components
-    Wk_Duration_Seconds=Time_Comp[0]*86400 + Time_Comp[1]*3600+ Time_Comp[2]*60 + Time_Comp[3] + Time_Comp[4]/1000
-    print('Total Workout Duration in Seconds: '+str(Wk_Duration_Seconds))
-
-    
     #Average Heart Rate During Workout
     Avg_HR=round(df_records_HR_Wk['@value'].mean(),2)
     print('Avg. Heart Rate: '+str(Avg_HR)+' BPM')
-
-    #Convert to int for x range
-    Wk_Duration_Seconds=int(Wk_Duration_Seconds)
-
-    #Create x range for plot
-    #Length of workout duration (in s) spaced out at intervals of 120 (s)
-    xlength=len(range(0,Wk_Duration_Seconds,120))
-    #Length of df with HR from desired workout
-    df_len=len(df_records_HR_Wk)
-
-    #Equation to ensure the correct # of xticks are created to space intervals of 120s
-    Diff=(round(df_len/xlength)*xlength)-df_len
-
-    x=range(0,df_len+Diff,round(df_len/xlength))
-    xlabel=range(0,Wk_Duration_Seconds,120)
-
-    #Plot HR during Workout
-    ax=df_records_HR_Wk['@value'].plot()
-    ax.set_xticks(x)
-    ax.set_xticklabels([x for x in xlabel])
-    plt.xlabel('Time (s)')
-    plt.ylabel('Beats Per Minute (BPM)')
-    plt.title('Heart Rate for '+Wk_Data[3])
-    plt.show()
 
     #Raname column to match activity 
     df_records_HR_Wk.rename(columns={'@value':Wk_Data[3]},inplace=True)
@@ -116,6 +91,7 @@ def extract_HR(Wk_Data):
 
 #Call functions
 Wk_Data=[]
+Wk_Duration=[]
 Wk_Type=[]
 
 #Iterate through all the workouts and save key information (Type, Start & End Times, Total Duration) into Wk_Data
@@ -125,8 +101,10 @@ for i in range(len(df_workouts)):
 #Save Workout types into a list to reference when training the model (saved into Wk_Type)
 for j in range(len(df_workouts)):
     Wk_List=Wk_Data[j]
+    Wk_Duration.append(Wk_List[2])
     Wk_Type.append(Wk_List[3])
 
+print(Wk_Duration)
 print(Wk_Type)
 
 #Extract HR data for all the workouts and save into 'HR MASTER' df
@@ -137,18 +115,28 @@ for k in range(len(df_workouts)):
     df_HR_MASTER.append(data)
 
 df_HR_MASTER=pd.concat(df_HR_MASTER, axis=1)
-
-#Plot all the workout data
 print(df_HR_MASTER)
 
-#Add the proper axis here 
-#Find longest workout duration -> divide it up at intervals of 120s or 240s -> See lines 92-100
-#Add obtain workout duration to line 43 in extract_workout instead of extract_HR
+#PLOTTING
+#Create x range for plot
+#Length of max workout duration (in s) spaced out at intervals of 120 (s)
+xlength=len(range(0,max(Wk_Duration),120))
+#Length of df with all HRs from workouts
+df_len=len(df_HR_MASTER)
 
-df_HR_MASTER.plot()
+#Equation to ensure the correct # of xticks are created to space intervals of 120s
+Diff=(round(df_len/xlength)*xlength)-df_len
+
+x=range(0,df_len+Diff,round(df_len/xlength))
+xlabel=range(0,max(Wk_Duration),120)
+
+ax=df_HR_MASTER.plot()
+ax.set_xticks(x)
+ax.set_xticklabels([x for x in xlabel])
+plt.xlabel('Time (s)')
+plt.ylabel('Beats Per Minute (BPM)')
+plt.title('Heart Rate Data for Workouts')
 plt.show()
-
-#Clean up the axis of the "Master Plot"
 
 #Try to plot a bunch of different workouts on the same plot and see how they differ
 # Look into algorithm to reduce noise (or smooth) signal  
